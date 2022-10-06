ebe7a32dd822b73b29f721986c924a4bfc100633
Chen Jian Yu <69144976+CJianYu98@users.noreply.github.com>
2022-10-07 01:35:09 +0800

    Update tut1_part1.py

--- tutorial/tutorial/spiders/tut1_part1.py
+++ tutorial/tutorial/spiders/tut1_part1.py
@@ -1,3 +1,4 @@
+from email.quoprimime import quote
 import scrapy
 
 
@@ -6,8 +7,10 @@ class QuotespiderSpider(scrapy.Spider):
     start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
-        quote_list = response.css("div.quote")
+        # quote_list = response.css("div.quote")
+        quote_list = response.xpath('//div[@class="quote"]')
         for quote in quote_list:
+            # Using CSS selector
             title = quote.css("span.text::text").get().replace("\u201c", "").replace("\u201d", "")
 
             author = quote.css("small.author::text").get()
@@ -16,5 +19,10 @@ class QuotespiderSpider(scrapy.Spider):
             tags = [tag.css("::text").get() for tag in tags_ls]
             # tags = quote.css('meta.keywords').attrib['content']
 
-            # Create a dictionary of information that we want our from one product
+            # Using Xpath
+            # title = quote.xpath('span[@class="text"]/text()').get()
+            # author = quote.xpath('span/small[@class="author"]/text()').get()
+            # tags = quote.xpath("div[@class='tags']/a[@class='tag']/text()").getall()
+
+            #     # Create a dictionary of information that we want our from one product
             yield {"title": title, "author": author, "tags": tags}
