34b4a4d635fd68f93303f8870f483d6e5bd22596
Chen Jian Yu <69144976+CJianYu98@users.noreply.github.com>
2022-10-07 00:21:19 +0800

    Update tut1_part1.py

--- tutorial/tutorial/spiders/tut1_part1.py
+++ tutorial/tutorial/spiders/tut1_part1.py
@@ -9,15 +9,11 @@ class QuotespiderSpider(scrapy.Spider):
         quote_list = response.css("div.quote")
         for quote in quote_list:
             title = quote.css("span.text::text").get().replace("\u201c", "").replace("\u201d", "")
-            # title = quote.css('span.text::text').get().replace("\u201c","").replace("\u201d","")
 
             author = quote.css("small.author::text").get()
-            # author = quote.css('small.author::text').get()
 
-            tags = quote.css("meta.keywords a.tag")
-            for tag in tags:
-                print(tag.css("::text").get())
-            # tags = quote.css('meta.keywords').attrib['content']
+            tags_ls = quote.css("a.tag")
+            tags = [tag.css("::text").get() for tag in tags_ls]
 
             # Create a dictionary of information that we want our from one product
             yield {"title": title, "author": author, "tags": tags}
